ec75ad85db1a065b6c48d161b42ea2821d474fe4
Kyrah Wiggins <kyrah.wiggins@gmail.com>
2020-03-11 04:17:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,16 +18,16 @@ def monopoly
 end	end
 
 
-def monopoly_with_second_tier	
+def monopoly_with_second_tier
 			monopoly = {
 	  :transit =>
 	{
 	:pieces => 4,
 	:rent_in_dollars => 20}
-end	
+end
 
 
-def monopoly_with_third_tier	
+def monopoly_with_third_tier
 		monopoly = {
 	  :transit =>
 	{
@@ -35,10 +35,10 @@ def monopoly_with_third_tier
 	:rent_in_dollars => 20}
 
 
-end	
+end
 
 
-def monopoly_with_fourth_tier	
+def monopoly_with_fourth_tier
 	monopoly = {
 	  :transit =>
 	{
